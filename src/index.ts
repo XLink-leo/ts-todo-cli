c05e9034580a9dc51ee114bfc478c9667a049b76
XLink-leo <2352257614@qq.com>
2026-05-09 18:52:51 +0800

    feat: 增加本地JSON持久化，重启数据不丢失

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 interface Todo {
   id: number;
   content: string;
   done: boolean;
 }
 
+// 数据文件路径
+const DATA_FILE = path.join(__dirname, '../todos.json');
 let todoList: Todo[] = [];
 
+// 从文件加载待办
+function loadTodos() {
+  try {
+    if (fs.existsSync(DATA_FILE)) {
+      const raw = fs.readFileSync(DATA_FILE, 'utf8');
+      todoList = JSON.parse(raw);
+    }
+  } catch (e) {
+    todoList = [];
+  }
+}
+
+// 保存待办到文件
+function saveTodos() {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(todoList, null, 2), 'utf8');
+}
+
+// 展示所有待办
 function showTodos() {
   console.log("\n===== 我的待办清单 =====");
   if (todoList.length === 0) {
@@ -18,6 +41,7 @@ function showTodos() {
   });
 }
 
+// 新增待办
 function addTodo(content: string) {
   const newTodo: Todo = {
     id: Date.now(),
@@ -25,14 +49,17 @@ function addTodo(content: string) {
     done: false
   };
   todoList.push(newTodo);
+  saveTodos();
   console.log("添加成功！");
   showTodos();
 }
 
+// 标记完成
 function finishTodo(id: number) {
   const todo = todoList.find(item => item.id === id);
   if (todo) {
     todo.done = true;
+    saveTodos();
     console.log("已标记为完成！");
   } else {
     console.log("找不到该待办ID");
@@ -40,11 +67,12 @@ function finishTodo(id: number) {
   showTodos();
 }
 
-// 新增：删除待办
+// 删除待办
 function deleteTodo(id: number) {
   const beforeLen = todoList.length;
   todoList = todoList.filter(item => item.id !== id);
   if (todoList.length < beforeLen) {
+    saveTodos();
     console.log("删除成功！");
   } else {
     console.log("找不到该待办ID");
@@ -53,6 +81,9 @@ function deleteTodo(id: number) {
 }
 
 function run() {
+  // 程序启动先加载数据
+  loadTodos();
+
   const args = process.argv.slice(2);
   const cmd = args[0];
 
